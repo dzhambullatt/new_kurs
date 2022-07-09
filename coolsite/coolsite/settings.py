64a2853817f69f31ab425e3590e107f60cc8ae50
djambulat <jambulatahmadov99@gmail.com>
2022-07-09 22:54:15 +0300

    updated templates

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -126,3 +126,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+###ключь от гита: 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
