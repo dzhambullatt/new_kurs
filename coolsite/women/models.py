9aa2886560a776a8147843216221f840a8ddaf1b
djambulat <jambulatahmadov99@gmail.com>
2022-07-17 19:12:53 +0300

    created new model

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -15,3 +15,10 @@ class Women(models.Model):
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, db_index=True)
+
+    def __str__(self):
+        return self.name
