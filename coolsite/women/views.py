67b0108feefaaa2e86acb8a1dedc7d7ecb9c42e9
djambulat <jambulatahmadov99@gmail.com>
2022-07-21 16:37:05 +0300

    updated views.py

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -45,5 +45,16 @@ def show_post(request, post_id):
 
 
 def show_category(request, cat_id):
-    return HttpResponse(f"Отображение категории с id = {cat_id}")
+    posts = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Отображение по рубрикам',
+        'cat_selected': cat_id,
+    }
+
+    return render(request, 'women/index.html', context=context)
 
