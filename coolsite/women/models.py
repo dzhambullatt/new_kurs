74bdc7bf1bbbfc28bd767eeeeb0aaf60278f3e72
djambulat <jambulatahmadov99@gmail.com>
2022-08-20 15:36:57 +0300

    updated admin.py

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -3,13 +3,13 @@ from django.urls import reverse
 
 
 class Women(models.Model):
-    title = models.CharField(max_length=250)
-    content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    title = models.CharField(max_length=250, verbose_name="Заголовок")
+    content = models.TextField(blank=True, verbose_name="Контент")
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Дата обнавления")
+    is_published = models.BooleanField(default=True, verbose_name="Опубликовано")
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name="Категория")
 
     def __str__(self):
         return self.title
@@ -20,13 +20,19 @@ class Women(models.Model):
     class Meta:
         verbose_name = 'Известные Женщины'
         verbose_name_plural = 'Известные Женщины'
+        ordering = ['time_create', 'title']
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=150, db_index=True)
+    name = models.CharField(max_length=150, db_index=True, verbose_name="Название")
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Категорию'
+        verbose_name_plural = 'Категории'
+        ordering = ['id']
