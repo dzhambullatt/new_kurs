46fb2a0c2959ed7db35425933df99ae70bb1c57d
djambulat <jambulatahmadov99@gmail.com>
2022-08-20 15:54:00 +0300

    created list_filter field

--- coolsite/women/admin.py
+++ coolsite/women/admin.py
@@ -8,6 +8,7 @@ class WomenAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
+    list_filter = ('is_published', 'time_create')
 
 
 class CategoryAdmin(admin.ModelAdmin):
