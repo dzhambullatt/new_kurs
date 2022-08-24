3ef4ca25b8c0c7ae6c01cf664f7c5a888954e2e0
djambulat <jambulatahmadov99@gmail.com>
2022-08-24 20:51:17 +0300

    updated models & db

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -4,12 +4,13 @@ from django.urls import reverse
 
 class Women(models.Model):
     title = models.CharField(max_length=250, verbose_name="Заголовок")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
     content = models.TextField(blank=True, verbose_name="Контент")
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото")
     time_create = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     time_update = models.DateTimeField(auto_now=True, verbose_name="Дата обнавления")
     is_published = models.BooleanField(default=True, verbose_name="Опубликовано")
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name="Категория")
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name="Категория")
 
     def __str__(self):
         return self.title
@@ -25,6 +26,7 @@ class Women(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=150, db_index=True, verbose_name="Название")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
 
     def __str__(self):
         return self.name
