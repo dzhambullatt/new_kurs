b5d8214701f4332b59978019a1141f8b94851b02
djambulat <jambulatahmadov99@gmail.com>
2022-08-30 21:45:53 +0300

    updated addpage.html

--- coolsite/women/forms.py
+++ coolsite/women/forms.py
@@ -6,6 +6,6 @@ class AddPostForm(forms.Form):
     title = forms.CharField(max_length=250, label="Заголовок")
     slug = forms.SlugField(max_length=250, label="URL")
     content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}), label="Контент")
-    is_published = forms.BooleanField(label="Публикация")
-    cat = forms.ModelChoiceField(queryset=Category.objects.all(), label="Категории")
+    is_published = forms.BooleanField(label="Публикация", required=False, initial=True)
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(), label="Категории", empty_label="Категория не выбрана")
 
