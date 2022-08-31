52c5e6a8733b85128207da656b985b4808574bcc
djambulat <jambulatahmadov99@gmail.com>
2022-08-31 23:27:01 +0300

    updated forms.py

--- coolsite/women/forms.py
+++ coolsite/women/forms.py
@@ -6,3 +6,7 @@ class AddPostForm(forms.ModelForm):
     class Meta:
         model = Women
         fields = ['title', 'slug', 'content', 'is_published', 'cat']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            'content': forms.Textarea(attrs={'cols': 60, 'rows': 10}),
+        }
