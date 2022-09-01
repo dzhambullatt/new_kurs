cd736a5ad46d4d8082658403908f2405111addad
djambulat <jambulatahmadov99@gmail.com>
2022-09-01 17:32:47 +0300

    updated vievs.py created class womencategory for category

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -21,7 +21,12 @@ class WomenHome(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['menu'] = menu
+        context['title'] = 'Главная страница'
+        context['cat_selected'] = 0
         return context
+
+    def get_queryset(self):
+        return Women.objects.filter(is_published=True)
 
 # def index(request):
 #     posts = Women.objects.all()
@@ -58,17 +63,25 @@ def login(request):
     return HttpResponse("Авторизация")
 
 
-def show_post(request, post_slug):
-    post = get_object_or_404(Women, slug=post_slug)
+class WomenCategory(ListView):
+    model = Women
+    template_name = 'women/index.html'
+    context_object_name = 'posts'
 
-    context = {
-        'post': post,
-        'menu': menu,
-        'title': post.title,
-        'cat_selected': post.cat_id,
-    }
+    def get_queryset(self):
+        return Women.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
 
-    return render(request, 'women/post.html', context=context)
+# def show_post(request, post_slug):
+#     post = get_object_or_404(Women, slug=post_slug)
+#
+#     context = {
+#         'post': post,
+#         'menu': menu,
+#         'title': post.title,
+#         'cat_selected': post.cat_id,
+#     }
+#
+#     return render(request, 'women/post.html', context=context)
 
 
 def show_category(request, cat_id):
